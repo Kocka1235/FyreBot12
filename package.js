c5655e51bef917e9995a088d2830d932afa997e5
Kocka1235 <42372263+Kocka1235@users.noreply.github.com>
2018-08-14 10:37:58 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "fyrebot",
+  "version": "1.0.0",
+  "description": "",
+  "main": "bot.js",
+  "scripts": {
+    "start":"node bot.js"
+  },
+    "dependencies": {
+    "discord.js": "^11.1.0"
+    "request":"2.81.0"
+  }
+}
